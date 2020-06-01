5515f0c46171c23b8c553708bf2fb031df39aaa5
Yaroslav Kaniev <y.kaniev@gmail.com>
2020-06-01 15:20:00 +0300

    rename src to docs, gh pages

--- gulpfile.js
+++ gulpfile.js
@@ -11,22 +11,22 @@ gulp.task('watch', gulp.series(function () {
        
         injectChanges: true,
         server: {
-            baseDir: "./src"
+            baseDir: "./docs"
         }
     });
-    var html = gulp.watch('src/index.html');
+    var html = gulp.watch('docs/index.html');
     html.on('change', function (path, stats) {
         console.log('you changed the html');
         browserSync.notify("Compiling, please wait!");
         browserSync.reload("index.html");
     })
-    var js = gulp.watch('src/js/*.js');
+    var js = gulp.watch('docs/js/*.js');
     js.on('change', function (path, stats) {
         console.log('you changed the js');
         browserSync.notify("Compiling, please wait!");
         browserSync.reload();
     })
-    var css = gulp.watch('src/css/*.css');
+    var css = gulp.watch('docs/css/*.css');
     css.on('change', function (path, stats) {
         console.log('you changed the css');
         browserSync.notify("Injecting CSS!");
